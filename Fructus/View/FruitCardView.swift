d10407ea60b5e95614d3cbc64b9611227918819f
limonikas@me.com <limonikas@me.com>
2021-09-17 01:35:35 +0300

    Styling text

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -13,17 +13,31 @@ struct FruitCardView: View {
     //  MARK: - BODY
 
     var body: some View {
-        VStack(spacing: 20) {
-            // FRUIT: IMAGE:
-            Image("blueberry")
-                .resizable()
-                .scaledToFit()
-                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 8, y: 8)
-            // FRUIT: TITLE
-            Text("Blueberry")
-            // FRUIT: HEADLINE
-            // BUTTON: START
-        } // VSTACK:
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE:
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 8, y: 8)
+                // FRUIT: TITLE
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x:  2, y: 2)
+                // FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutritious and wildly popular fruit all over the world.")
+                    .foregroundColor(Color(.white))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                // BUTTON: START
+            } // VSTACK:
+        } // ZSTACK:
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 
