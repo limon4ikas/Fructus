5493a8f6da05e41fc4eb4ecdbf47656ca1ad5486
limonikas@me.com <limonikas@me.com>
2021-09-18 02:16:22 +0300

    Detail fruct  header view

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct FruitDetailView: View {
     // MARK: - PROPERTIES
+
     var fruit: Fruit
     
     // MARK: - BODY
+
     var body: some View {
-        NavigationView() {
+        NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(alignment: .center, spacing: 20) {
                     // HEADER
@@ -41,7 +43,6 @@ struct FruitDetailView: View {
                             .multilineTextAlignment(.leading)
                         
                         // LINK
-                        
                     } //: VSTACK
                     .padding(.horizontal, 20)
                     .frame(maxWidth: 640, alignment: .center)
@@ -51,7 +52,8 @@ struct FruitDetailView: View {
     }
 }
 
-    // MARK: - PREVIEW
+// MARK: - PREVIEW
+
 struct FruitDetailView_Previews: PreviewProvider {
     static var previews: some View {
         FruitDetailView(fruit: fruitsData[0])
