06b7c8eec83425e529742b11337fa4f5d72779e3
limonikas@me.com <limonikas@me.com>
2021-09-18 00:39:54 +0300

    Fruit row component

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct OnboardingView: View {
     // MARK: - PROPERTIES
+    var fruits: [Fruit] = fruitData
 
     // MARK: - BODY
 
     var body: some View {
         TabView {
-            FruitCardView()
-            FruitCardView()
-            FruitCardView()
+            ForEach(fruits[0...5]) { fruit in
+                FruitCardView(fruit: fruit)
+            } //: LOOP
         } //: TABVIEW
         .tabViewStyle(PageTabViewStyle())
         .padding(.vertical, 20)
@@ -27,6 +28,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitData)
     }
 }
