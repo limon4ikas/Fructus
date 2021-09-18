6b0cb776e8b8d5e727caad942701da0d2ee04cdd
limonikas@me.com <limonikas@me.com>
2021-09-19 02:09:14 +0300

    Settings row view

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -32,12 +32,22 @@ struct SettingsView: View {
                                 .cornerRadius(9)
                             Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
                                 .font(.footnote)
-                        }
-                    }
+                        } //: HSTACK
+                    } //: GROUPBOX
 
                     // MARK: - SECTION 2
 
                     // MARK: - SECTION 3
+
+                    GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")) {
+                        SettingsRowView(name: "Developer", content: "John Smith")
+                        SettingsRowView(name: "Designer", content: "Rober Petras")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        SettingsRowView(name: "Website", linkLabel: "Homepage", linkDestination: "#")
+                        SettingsRowView(name: "Twitter", linkLabel: "@limonikas", linkDestination: "#")
+                        SettingsRowView(name: "Swift UI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                    } //: GROUPBOX
                 } //: VSTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
                 .navigationBarItems(trailing: Button(action: {
