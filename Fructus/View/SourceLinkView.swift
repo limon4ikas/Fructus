265e3eed345b9968b8a2cccb2e86c1298c2764cb
limonikas@me.com <limonikas@me.com>
2021-09-18 12:45:15 +0300

    Adding diclosure group to detail view

--- Fructus/View/SourceLinkView.swift
+++ Fructus/View/SourceLinkView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct SourceLinkView: View {
+    // MARK: - PROPERTIES
+
+    // MARK: - BODY
+
     var body: some View {
         GroupBox {
             HStack {
@@ -15,11 +19,13 @@ struct SourceLinkView: View {
                 Spacer()
                 Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
                 Image(systemName: "arrow.up.right.square")
-            }
+            } //: HSTACK
             .font(.footnote)
-        }
+        } //: GROUPBOX
     }
 }
+
+// MARK: - PREVIEW
 
 struct SourceLinkView_Previews: PreviewProvider {
     static var previews: some View {
